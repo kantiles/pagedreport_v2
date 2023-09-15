48827e867d39b3e1d2ccf4bc7e294a74fb929599
Thomas Vroylandt <tvroylandt@gmail.com>
2023-09-15 09:28:19 +0200

    Add theme as YAML option to change CSS

--- _extensions/pagedreport/pagedreport.lua
+++ _extensions/pagedreport/pagedreport.lua
@@ -7,6 +7,18 @@
 -- License: see license.md
 ----------------------------
 
+-- read file function
+-- from https://stackoverflow.com/questions/11201262/how-to-read-data-from-a-file-in-lua
+local open = io.open
+
+local function read_file(path)
+  local file = open(path, "rb")  -- r read mode and b binary mode
+  if not file then return nil end
+  local content = file:read "*a" -- *a or *all reads the whole file
+  file:close()
+  return content
+end
+
 -- function to create CSS variables with custom values
 function addPagedreportVars(meta)
   -- get meta param
@@ -31,10 +43,22 @@ function addPagedreportVars(meta)
       </style>
       ]]
 
+  -- get theme content
+  theme_css = [[
+    <style> ]]
+      .. read_file("_extensions/pagedreport/resources/css/" .. pandoc.utils.stringify(pagedreport['theme']) .. ".css") ..
+      [[
+      </style>
+  ]]
+
   -- read and add css to the current one
   local current_css = meta['header-includes']
-  
+
+  -- add variables
   current_css[#current_css + 1] = pandoc.MetaBlocks(pandoc.RawBlock("html", pagedreport_css))
+  -- add theme
+  current_css[#current_css + 1] = pandoc.MetaBlocks(pandoc.RawBlock("html", theme_css))
+
   meta['header-includes'] = current_css
 
   -- return metadata
